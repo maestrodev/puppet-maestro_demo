c1d0bc112a28d03d9790ae9f8909d03d45d46077
Doug Henderson <dhenderson@maestrodev.com>
2013-03-14 11:49:16 -0700

    Collabnet plugin has a different name

--- manifests/demo/composition/collabnet.pp
+++ manifests/demo/composition/collabnet.pp
@@ -1,7 +1,7 @@
 class maestro_demo::demo::composition::collabnet {
   # Add any specific deps here
   
-  maestro::plugin { 'maestro-collabnet-plugin':
+  maestro::plugin { 'collabnet-maestro-plugin':
                         version => '1.2' }
 
   maestro_demo::demo::composition::base { 'collabnet': }
