1e6eb88440b2c47e9fe67e253a7c91fd4cd0993b
Doug Henderson <dhenderson@maestrodev.com>
2013-03-14 11:23:26 -0700

    Use release version

--- manifests/demo/composition/collabnet.pp
+++ manifests/demo/composition/collabnet.pp
@@ -2,7 +2,7 @@ class maestro_demo::demo::composition::collabnet {
   # Add any specific deps here
   
   maestro::plugin { 'maestro-collabnet-plugin':
-                        version => '1.2-20130312.234708-9' }
+                        version => '1.2' }
 
   maestro_demo::demo::composition::base { 'collabnet': }
 }
