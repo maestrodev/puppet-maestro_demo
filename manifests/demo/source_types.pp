d95a45d74d227c0f88ed3f7852c7fda08be56b6b
Carlos Sanchez <csanchez@maestrodev.com>
2013-09-30 20:52:23 +0200

    source_types must be installed from plugins

--- manifests/demo/source_types.pp
+++ manifests/demo/source_types.pp
@@ -1,3 +1,4 @@
+# deprecated, source_types must be installed from plugins
 class maestro_demo::demo::source_types {
   # Add any source types here... ideally in the form
   # maestro_demo::source_type::<source_type_name>
